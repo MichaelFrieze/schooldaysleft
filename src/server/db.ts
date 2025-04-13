1f3179e4859db10a6b6852b873f7f5163360776c
Michael Frieze <michaelfrieze@icloud.com>
2025-04-12 20:14:09 -0400

    change: remove the new prisma-client generator since it's early access

--- src/server/db.ts
+++ src/server/db.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "../../prisma/generated/prisma/client";
+import { PrismaClient } from "@prisma/client";
 import { withAccelerate } from "@prisma/extension-accelerate";
 
 import { env } from "~/env";
