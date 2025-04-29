6dadb986e6155cc97527f42f420f0b2220755855
Michael Frieze <michaelfrieze@icloud.com>
2025-04-28 21:34:31 -0400

    feat: add drizzle cache with upstash

--- src/modules/posts/server/procedures.ts
+++ src/modules/posts/server/procedures.ts
@@ -2,6 +2,7 @@ import { z } from "zod";
 
 import { createTRPCRouter, publicProcedure } from "@/trpc/init";
 import { posts } from "@/db/schema";
+import { desc } from "drizzle-orm";
 
 export const postRouter = createTRPCRouter({
   hello: publicProcedure
@@ -21,10 +22,20 @@ export const postRouter = createTRPCRouter({
     }),
 
   getLatest: publicProcedure.query(async ({ ctx }) => {
-    const post = await ctx.db.query.posts.findFirst({
-      orderBy: (posts, { desc }) => [desc(posts.createdAt)],
-    });
+    // const post = await ctx.db.query.posts.findFirst({
+    //   orderBy: (posts, { desc }) => [desc(posts.createdAt)],
+    // });
+    // return post ?? null;
 
-    return post ?? null;
+    const post = await ctx.db
+      .select()
+      .from(posts)
+      .orderBy(desc(posts.createdAt))
+      .limit(1)
+      .$withCache({ autoInvalidate: false })
+      .execute()
+      .then((res) => res[0] ?? null);
+
+    return post;
   }),
 });
