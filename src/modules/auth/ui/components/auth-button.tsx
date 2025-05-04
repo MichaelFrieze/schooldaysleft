995ead8d61f7dca799476a59a005fb46c37400bb
Michael Frieze <michaelfrieze@icloud.com>
2025-05-04 15:48:55 -0400

    fix: ClerkLoaded and ClerkLoading is causing a hydration error

--- src/modules/auth/ui/components/auth-button.tsx
+++ src/modules/auth/ui/components/auth-button.tsx
@@ -5,45 +5,51 @@ import {
   SignedIn,
   SignedOut,
   UserButton,
-  ClerkLoading,
-  ClerkLoaded,
+  useAuth,
 } from "@clerk/nextjs";
 import { UserCircleIcon } from "lucide-react";
 
 import { Button } from "@/components/ui/button";
 import { Skeleton } from "@/components/ui/skeleton";
+import { useEffect, useState } from "react";
 
 export const AuthButton = () => {
+  const [isMounted, setIsMounted] = useState(false);
+  const { isLoaded } = useAuth();
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted || !isLoaded) {
+    return <Skeleton className="h-8 w-8 rounded-full" />;
+  }
+
   return (
     <div className="h-8 w-8">
-      <ClerkLoading>
-        <Skeleton className="h-8 w-8 rounded-full" />
-      </ClerkLoading>
-      <ClerkLoaded>
-        <SignedIn>
-          <UserButton
-            appearance={{
-              elements: {
-                avatarBox: {
-                  height: "2rem",
-                  width: "2rem",
-                },
+      <SignedIn>
+        <UserButton
+          appearance={{
+            elements: {
+              avatarBox: {
+                height: "2rem",
+                width: "2rem",
               },
-            }}
-          />
-        </SignedIn>
-        <SignedOut>
-          <SignInButton mode="modal">
-            <Button
-              variant="default"
-              className="h-8 rounded-full text-sm font-medium shadow-none"
-            >
-              <UserCircleIcon />
-              Sign in
-            </Button>
-          </SignInButton>
-        </SignedOut>
-      </ClerkLoaded>
+            },
+          }}
+        />
+      </SignedIn>
+      <SignedOut>
+        <SignInButton mode="modal">
+          <Button
+            variant="default"
+            className="h-8 rounded-full text-sm font-medium shadow-none"
+          >
+            <UserCircleIcon />
+            Sign in
+          </Button>
+        </SignInButton>
+      </SignedOut>
     </div>
   );
 };
