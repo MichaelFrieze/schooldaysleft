571a2726776c4c25e867fab38301e6b505a4a649
Michael Frieze <michaelfrieze@icloud.com>
2025-05-04 15:47:09 -0400

    fix: auth button causes some layout shift in the navbar between loading states

--- src/modules/auth/ui/components/auth-button.tsx
+++ src/modules/auth/ui/components/auth-button.tsx
@@ -5,51 +5,45 @@ import {
   SignedIn,
   SignedOut,
   UserButton,
-  useAuth,
+  ClerkLoading,
+  ClerkLoaded,
 } from "@clerk/nextjs";
 import { UserCircleIcon } from "lucide-react";
 
 import { Button } from "@/components/ui/button";
 import { Skeleton } from "@/components/ui/skeleton";
-import { useEffect, useState } from "react";
 
 export const AuthButton = () => {
-  const [isMounted, setIsMounted] = useState(false);
-  const { isLoaded } = useAuth();
-
-  useEffect(() => {
-    setIsMounted(true);
-  }, []);
-
-  if (!isMounted || !isLoaded) {
-    return <Skeleton className="h-8 w-8 rounded-full" />;
-  }
-
   return (
-    <>
-      <SignedIn>
-        <UserButton
-          appearance={{
-            elements: {
-              avatarBox: {
-                height: "2rem",
-                width: "2rem",
+    <div className="h-8 w-8">
+      <ClerkLoading>
+        <Skeleton className="h-8 w-8 rounded-full" />
+      </ClerkLoading>
+      <ClerkLoaded>
+        <SignedIn>
+          <UserButton
+            appearance={{
+              elements: {
+                avatarBox: {
+                  height: "2rem",
+                  width: "2rem",
+                },
               },
-            },
-          }}
-        />
-      </SignedIn>
-      <SignedOut>
-        <SignInButton mode="modal">
-          <Button
-            variant="default"
-            className="h-8 rounded-full text-sm font-medium shadow-none"
-          >
-            <UserCircleIcon />
-            Sign in
-          </Button>
-        </SignInButton>
-      </SignedOut>
-    </>
+            }}
+          />
+        </SignedIn>
+        <SignedOut>
+          <SignInButton mode="modal">
+            <Button
+              variant="default"
+              className="h-8 rounded-full text-sm font-medium shadow-none"
+            >
+              <UserCircleIcon />
+              Sign in
+            </Button>
+          </SignInButton>
+        </SignedOut>
+      </ClerkLoaded>
+    </div>
   );
 };
