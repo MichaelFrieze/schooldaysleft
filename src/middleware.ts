9bb28c377b9c56b4fa429870c2ec46778f62658b
Michael Frieze <michaelfrieze@icloud.com>
2025-06-16 10:20:18 -0400

    change: remove well-known from public routes in middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,12 +1,7 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
-const isPublicRoute = createRouteMatcher([
-  "/",
-  "/sign-in(.*)",
-  "/not-found",
-  "/.well-known(.*)",
-]);
+const isPublicRoute = createRouteMatcher(["/", "/sign-in(.*)", "/not-found"]);
 
 export default clerkMiddleware(async (auth, req) => {
   const { userId } = await auth();
