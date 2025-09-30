9a97dff0563a704ada27179b64e8e17f0db9247d
Michael Frieze <michaelfrieze@icloud.com>
2025-09-29 20:50:53 -0400

    fix: getting an occasional unauthenticated error in Convex

--- src/routes/(countdown)/dashboard/index.tsx
+++ src/routes/(countdown)/dashboard/index.tsx
@@ -1,14 +1,12 @@
 import { DashboardView } from "@/modules/dashboard/ui/views/dashboard-view";
-import { convexQuery } from "@convex-dev/react-query";
 import { createFileRoute } from "@tanstack/react-router";
-import { api } from "convex/_generated/api";
 
 export const Route = createFileRoute("/(countdown)/dashboard/")({
-	loader: (opts) => {
-		opts.context.queryClient.prefetchQuery(
-			convexQuery(api.countdowns.getAll, {}),
-		);
-	},
+	// loader: (opts) => {
+	// 	opts.context.queryClient.prefetchQuery(
+	// 		convexQuery(api.countdowns.getAll, {}),
+	// 	);
+	// },
 	ssr: false,
 	component: DashboardRoute,
 });
--- src/routes/(countdown)/route.tsx
+++ src/routes/(countdown)/route.tsx
@@ -1,14 +1,12 @@
 import { CountdownLayout } from "@/modules/countdown/ui/layouts/countdown-layout";
-import { convexQuery } from "@convex-dev/react-query";
 import { Outlet, createFileRoute } from "@tanstack/react-router";
-import { api } from "convex/_generated/api";
 
 export const Route = createFileRoute("/(countdown)")({
-	loader: (opts) => {
-		opts.context.queryClient.prefetchQuery(
-			convexQuery(api.countdowns.getAll, {}),
-		);
-	},
+	// loader: (opts) => {
+	// 	opts.context.queryClient.prefetchQuery(
+	// 		convexQuery(api.countdowns.getAll, {}),
+	// 	);
+	// },
 	ssr: false,
 	component: CountdownLayoutRoute,
 });
