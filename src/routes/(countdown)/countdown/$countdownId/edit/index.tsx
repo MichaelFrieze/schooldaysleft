a320354f1bf422aa33bd761bd02f117f357c49c4
Michael Frieze <michaelfrieze@icloud.com>
2025-09-29 21:03:44 -0400

    fix: getting an occasional unauthenticated error in Convex

--- src/routes/(countdown)/countdown/$countdownId/edit/index.tsx
+++ src/routes/(countdown)/countdown/$countdownId/edit/index.tsx
@@ -1,19 +1,16 @@
 import { EditCountdownView } from "@/modules/edit-countdown/ui/views/edit-countdown-view";
-import { convexQuery } from "@convex-dev/react-query";
 import { createFileRoute } from "@tanstack/react-router";
-import { api } from "convex/_generated/api";
-import type { Id } from "convex/_generated/dataModel";
 
 export const Route = createFileRoute(
 	"/(countdown)/countdown/$countdownId/edit/",
 )({
-	loader: (opts) => {
-		opts.context.queryClient.prefetchQuery(
-			convexQuery(api.countdowns.getById, {
-				id: opts.params.countdownId as Id<"countdowns">,
-			}),
-		);
-	},
+	// loader: (opts) => {
+	// 	opts.context.queryClient.prefetchQuery(
+	// 		convexQuery(api.countdowns.getById, {
+	// 			id: opts.params.countdownId as Id<"countdowns">,
+	// 		}),
+	// 	);
+	// },
 	ssr: false,
 	component: EditCountdownRoute,
 });
--- src/routes/(countdown)/dashboard/index.tsx
+++ src/routes/(countdown)/dashboard/index.tsx
@@ -2,11 +2,6 @@ import { DashboardView } from "@/modules/dashboard/ui/views/dashboard-view";
 import { createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/(countdown)/dashboard/")({
-	// loader: (opts) => {
-	// 	opts.context.queryClient.prefetchQuery(
-	// 		convexQuery(api.countdowns.getAll, {}),
-	// 	);
-	// },
 	ssr: false,
 	component: DashboardRoute,
 });
